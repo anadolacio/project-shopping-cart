757a4252ec720eb449b2803d4d5c36ea8ecfe021
anadolacio <anadolacio@hotmail.com>
2022-11-17 17:41:00 -0300

    resolução dos requisitos 4 e 5

--- src/main.js
+++ src/main.js
@@ -8,13 +8,33 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionListProducts = document.querySelector('.products');
 
 // Functions
+const loadingAlert = () => {
+  const message = document.createElement('p');
+  message.classList.add('loading');
+  message.innerHTML = 'carregando...';
+  sectionListProducts.appendChild(message);
+};
+
+const APIerror = () => {
+  const message = document.createElement('p');
+  message.classList.add('error');
+  message.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  sectionListProducts.appendChild(message);
+};
 
 const createProductList = async () => {
-  const equipments = await fetchProductsList('computador');
-  equipments.forEach((equipment) => {
-    const item = createProductElement(equipment);
-    sectionListProducts.appendChild(item);
-  });
+  loadingAlert();
+  try {
+    const equipments = await fetchProductsList('computador');
+    equipments.forEach((equipment) => {
+      const item = createProductElement(equipment);
+      sectionListProducts.appendChild(item);
+    });
+    const removeLoading = document.querySelector('.loading');
+    removeLoading.remove();
+  } catch {
+    return APIerror();
+  }
 };
 
 createProductList();
