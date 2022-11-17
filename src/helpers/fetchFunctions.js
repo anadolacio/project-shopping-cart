d801c1738acc50460e892eb67d3073a85001773a
anadolacio <anadolacio@hotmail.com>
2022-11-17 12:51:22 -0300

    resolução requisito 7

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,14 +1,23 @@
-export const fetchProduct = () => {
-// codigo
+export const fetchProduct = async (id) => {
+  const urlId = `https://api.mercadolibre.com/items/${id}`;
+  if (!id) throw Error('ID não informado');
+  try {
+    const response = await fetch(urlId);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export const fetchProductsList = async (query) => {
   const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  if (!query) throw Error('Termo de busca não informado');
   try {
     const response = await fetch(urlQuery);
     const data = await response.json();
     return data.results;
-  } catch {
-    throw Error('Termo de busca não informado');
+  } catch (error) {
+    return error.message;
   }
 };
--- src/main.js
+++ src/main.js
@@ -3,8 +3,39 @@ import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
+// Search html
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionListProducts = document.querySelector('.produtcs');
 
-console.log(fetchProductsList('computador'));
+// Functions
 
-createProductElement();
+const loadingAlert = () => {
+  const message = document.createElement('p');
+  message.classList.add('loading');
+  message.innerHTML = 'carregando...';
+  sectionListProducts.appendChild(message);
+};
+
+const APIerror = () => {
+  const message = document.createElement('p');
+  message.classList.add('error');
+  message.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  sectionListProducts.appendChild(message);
+};
+
+const createProductList = async () => {
+  try {
+    sectionListProducts.appendChild(loadingAlert);
+    const products = await fetchProductsList('computador');
+    products.forEach((product) => {
+      const { id, title, thumbnail, price } = product;
+      sectionListProducts.appendChild(
+        createProductElement({ id, title, thumbnail, price }),
+      );
+    });
+    const removeLoading = document.querySelector('.loading');
+    removeLoading.remove();
+  } catch (error) {
+    return APIerror();
+  }
+};
